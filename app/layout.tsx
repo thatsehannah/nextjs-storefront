90b02279210e7cae881c8defb147a3ebcb6d23a8
E. Hannah <echannah631@gmail.com>
2024-12-03 15:00:25 -0500

    changed more boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from 'next';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: 'Next Store',
   description: 'A nifty store built with Next.js',
 };
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <main className='max-w-3xl mx-auto py-10'>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
+
 const HomePage = () => {
-  return <h1 className='text-3xl'>HomePage</h1>;
+  return <h1 className='text-3xl'>Home Page</h1>;
 };
 export default HomePage;
