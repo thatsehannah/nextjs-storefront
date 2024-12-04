2b6096a67d58db05b453615ddf40296691235344
E. Hannah <echannah631@gmail.com>
2024-12-03 21:01:55 -0500

    creating components for navbar, removing shadcn button from HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
       <h1 className='text-3xl'>Home Page</h1>
-      <Button
-        variant='outline'
-        size='lg'
-        className='m-8'
-      >
-        Click Me
-      </Button>
     </div>
   );
 };
