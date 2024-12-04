790750d9dca66310c8264ccfd45836ee508768b5
E. Hannah <echannah631@gmail.com>
2024-12-04 15:03:25 -0500

    practicing a few crud operations using the prisma instance in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import db from '@/utils/db';
 
-const AboutPage = () => {
-  return <div>About Page</div>;
+const AboutPage = async () => {
+  await db.testProfile.create({
+    data: {
+      name: 'random name',
+    },
+  });
+
+  const users = await db.testProfile.findMany();
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <h2
+            key={user.id}
+            className='text-2xl font-bold'
+          >
+            {user.name}
+          </h2>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AboutPage;
