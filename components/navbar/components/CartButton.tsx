9a9beb7b65054057caccdb4f754950ae3b30cc2b
E. Hannah <echannah631@gmail.com>
2024-12-03 21:54:26 -0500

    creating a cart button with span to show num of items in cart

--- components/navbar/components/CartButton.tsx
+++ components/navbar/components/CartButton.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { LuShoppingCart } from 'react-icons/lu';
 
-const CartButton = () => {
-  return <div>CartButton</div>;
+const CartButton = async () => {
+  const numItemsInCart = 6; //temp value; will be doing a database query for this value later
+
+  return (
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='flex justify-center items-center relative'
+    >
+      <Link href='/cart'>
+        <LuShoppingCart />
+        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 };
 
 export default CartButton;
