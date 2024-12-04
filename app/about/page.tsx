047380944304bcbcf5120b2c93254130a60a5a22
E. Hannah <echannah631@gmail.com>
2024-12-04 15:14:56 -0500

    creating a real model for our project

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,28 +1,7 @@
 import React from 'react';
-import db from '@/utils/db';
 
-const AboutPage = async () => {
-  await db.testProfile.create({
-    data: {
-      name: 'random name',
-    },
-  });
-
-  const users = await db.testProfile.findMany();
-  return (
-    <div>
-      {users.map((user) => {
-        return (
-          <h2
-            key={user.id}
-            className='text-2xl font-bold'
-          >
-            {user.name}
-          </h2>
-        );
-      })}
-    </div>
-  );
+const AboutPage = () => {
+  return <div>About Page</div>;
 };
 
 export default AboutPage;
