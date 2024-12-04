da774fc7a3780a53ea13ce726a1a83581463b572
E. Hannah <echannah631@gmail.com>
2024-12-03 21:41:53 -0500

    installing react-icons library, creating logo component

--- components/navbar/components/Logo.tsx
+++ components/navbar/components/Logo.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { VscCode } from 'react-icons/vsc';
 
 const Logo = () => {
-  return <div>Logo</div>;
+  return (
+    <Button
+      size='icon'
+      asChild
+    >
+      <Link href='/'>
+        <VscCode className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 };
 
 export default Logo;
