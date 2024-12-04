29b42c24b687eec357155c6e26f346e084032715
E. Hannah <echannah631@gmail.com>
2024-12-04 14:02:57 -0500

    creating dropdown component for links menu, mapping over a links array to create dropdown menu items

--- components/navbar/components/LinksDropDown.tsx
+++ components/navbar/components/LinksDropDown.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import Link from 'next/link';
+import { links } from '@/utils/links';
+import { Button } from '@/components/ui/button';
+import { AlignLeft } from 'lucide-react';
 
-const LinksDropDown = () => {
-  return <div>LinksDropDown</div>;
-};
-
-export default LinksDropDown;
+function LinksDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant='outline'
+          className='flex gap-4 max-w-[100px]'
+        >
+          <AlignLeft />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-40'
+        align='start'
+        sideOffset={10}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link
+                href={link.href}
+                className='capitalize w-full'
+              >
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+        <DropdownMenuSeparator />
+        {/* Temporary placeholder for logout button 👇🏾 will implement logout functionality later */}
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+export default LinksDropdown;
