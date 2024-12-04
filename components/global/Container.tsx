8845dd5c5302a9dfb41b9ee72b06c95715b50e31
E. Hannah <echannah631@gmail.com>
2024-12-03 21:08:50 -0500

    creating a global container component

---
+++ components/global/Container.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
+
+type ContainerProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
