b69a2905c086cdf4e88f6cbb8899e6f947d6de26
E. Hannah <echannah631@gmail.com>
2024-12-09 12:37:10 -0500

    creating static about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <section>
+      <h1 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl'>
+        We love
+        <span className='bg-primary py-4 px-4 rounded-lg tracking-widest text-white'>
+          store
+        </span>
+      </h1>
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ipsum illo
+        fuga totam fugiat, unde nam, quos et obcaecati, accusamus sit quam
+        numquam veritatis aspernatur quasi architecto reprehenderit repellat
+        natus.
+      </p>
+    </section>
+  );
 };
 
 export default AboutPage;
