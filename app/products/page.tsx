e8790c7dfaed29cf9aabc9fdf511ef35408169a9
E. Hannah <echannah631@gmail.com>
2024-12-09 14:16:29 -0500

    creating header in products container page, refactoring search params layout property to literal union in products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 type SearchParams = {
   searchParams: {
-    layout?: string;
+    layout?: 'grid' | 'list';
     search?: string;
   };
 };
--- components/products/components/ProductsContainer.tsx
+++ components/products/components/ProductsContainer.tsx
@@ -1,12 +1,72 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { fetchAllProducts } from '@/utils/actions';
+import { LayoutGrid, LayoutList } from 'lucide-react';
+import Link from 'next/link';
+import React, { ReactNode } from 'react';
 
 type ProductsContainerProps = {
-  layout: string;
+  layout: 'grid' | 'list';
   search: string;
 };
 
-const ProductsContainer = ({ layout, search }: ProductsContainerProps) => {
-  return <div>ProductsContainer</div>;
+type LayoutButtonProps = ProductsContainerProps & {
+  layoutIcon: ReactNode;
+  variant: 'default' | 'ghost';
+};
+
+const LayoutButton = ({
+  layout,
+  search,
+  layoutIcon,
+  variant,
+}: LayoutButtonProps) => {
+  return (
+    <Button
+      size='icon'
+      variant={variant}
+      asChild
+    >
+      <Link href={`/products?layout=${layout}${search}`}>{layoutIcon}</Link>
+    </Button>
+  );
+};
+
+const ProductsContainer = async ({
+  layout,
+  search,
+}: ProductsContainerProps) => {
+  const products = await fetchAllProducts();
+  const searchTerm = search ? `&search=${search}` : '';
+
+  return (
+    <>
+      {/* HEADER */}
+      <section>
+        <div className='flex justify-between items-center'>
+          <h4 className='font-medium text-lg'>
+            {products.length} product{products.length > 1 && 's'}
+          </h4>
+          <div className='flex gap-x-4'>
+            {/* LAYOUT BUTTONS */}
+            <LayoutButton
+              layout='grid'
+              variant={layout === 'grid' ? 'default' : 'ghost'}
+              search={search}
+              layoutIcon={<LayoutGrid />}
+            />
+            <LayoutButton
+              layout='list'
+              variant={layout === 'list' ? 'default' : 'ghost'}
+              search={search}
+              layoutIcon={<LayoutList />}
+            />
+          </div>
+        </div>
+        <Separator className='mt-4' />
+      </section>
+    </>
+  );
 };
 
 export default ProductsContainer;
