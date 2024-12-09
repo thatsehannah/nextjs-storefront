8115045cdba5adb62d200b8935180d3f29b6d244
E. Hannah <echannah631@gmail.com>
2024-12-09 12:20:40 -0500

    began setting up hero section

--- components/home/components/Hero.tsx
+++ components/home/components/Hero.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import HeroCarousel from './HeroCarousel';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+      <div>
+        <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl'>
+          We are changing the way people shop
+        </h1>
+        <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta totam
+          pariatur suscipit quae earum impedit itaque nisi nobis repellendus
+          blanditiis!
+        </p>
+        <Button
+          asChild
+          size='lg'
+          className='mt-10 capitalize'
+        >
+          <Link href='/products'>our products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 };
 
 export default Hero;
