7fc0263ddf30954fc747699316dc4ea4778d11ea
E. Hannah <echannah631@gmail.com>
2024-12-09 14:41:15 -0500

    moving products list outside of section in products container, changing styling of total num of products

--- components/products/components/ProductsContainer.tsx
+++ components/products/components/ProductsContainer.tsx
@@ -4,6 +4,8 @@ import { fetchAllProducts } from '@/utils/actions';
 import { LayoutGrid, LayoutList } from 'lucide-react';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
+import ProductsGrid from './ProductsGrid';
+import ProductsList from './ProductsList';
 
 type ProductsContainerProps = {
   layout: 'grid' | 'list';
@@ -37,6 +39,7 @@ const ProductsContainer = async ({
   search,
 }: ProductsContainerProps) => {
   const products = await fetchAllProducts();
+  const totalNumOfProducts = products.length;
   const searchTerm = search ? `&search=${search}` : '';
 
   return (
@@ -45,7 +48,10 @@ const ProductsContainer = async ({
       <section>
         <div className='flex justify-between items-center'>
           <h4 className='font-medium text-lg'>
-            {products.length} product{products.length > 1 && 's'}
+            <span className='text-white rounded-full px-3 py-1 mr-2 bg-foreground'>
+              {totalNumOfProducts}
+            </span>
+            product{totalNumOfProducts > 1 && 's'}
           </h4>
           <div className='flex gap-x-4'>
             {/* LAYOUT BUTTONS */}
@@ -65,6 +71,19 @@ const ProductsContainer = async ({
         </div>
         <Separator className='mt-4' />
       </section>
+
+      {/* LIST OF PRODUCTS */}
+      <div>
+        {totalNumOfProducts === 0 ? (
+          <h5 className='text-2xl mt-16'>
+            Sorry, no products matched your search...
+          </h5>
+        ) : layout === 'grid' ? (
+          <ProductsGrid products={products} />
+        ) : (
+          <ProductsList products={products} />
+        )}
+      </div>
     </>
   );
 };
