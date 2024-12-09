32602ecbd5783516be43cf78f51f95290d67cae1
E. Hannah <echannah631@gmail.com>
2024-12-09 10:39:06 -0500

    finishing emptylist and sectiontitle components

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Separator } from '../ui/separator';
 
-const SectionTitle = () => {
-  return <div>SectionTitle</div>;
+type SectionTitleProps = {
+  text: string;
+};
+
+const SectionTitle = ({ text }: SectionTitleProps) => {
+  return (
+    <div>
+      <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 };
 
 export default SectionTitle;
