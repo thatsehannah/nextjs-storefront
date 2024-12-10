a2aec6e6801a065c724ac1cd201abf1bd536dc1f
E. Hannah <echannah631@gmail.com>
2024-12-10 16:59:47 -0500

    removing unneccessary components folder within product folder

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Image from 'next/image';
 import { fetchProductById } from '@/utils/actions';
 import { formatCurrency } from '@/utils/format';
-import BreadCrumbs from '@/components/product/components/BreadCrumbs';
+import BreadCrumbs from '@/components/product/BreadCrumbs';
 import FavoriteToggleButton from '@/components/products/components/FavoriteToggleButton';
-import AddToCartButton from '@/components/product/components/AddToCartButton';
-import ProductRating from '@/components/product/components/ProductRating';
+import AddToCartButton from '@/components/product/AddToCartButton';
+import ProductRating from '@/components/product/ProductRating';
 
 const ProductDetailsPage = () => {
   return <div>ProductDetailsPage</div>;
