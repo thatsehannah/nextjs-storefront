97b782e8d63fb6cb56a718df65faedd1f72d1697
E. Hannah <echannah631@gmail.com>
2024-12-10 16:58:44 -0500

    stubbing out dynamic single product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from 'next/image';
+import { fetchProductById } from '@/utils/actions';
+import { formatCurrency } from '@/utils/format';
+import BreadCrumbs from '@/components/product/components/BreadCrumbs';
+import FavoriteToggleButton from '@/components/products/components/FavoriteToggleButton';
+import AddToCartButton from '@/components/product/components/AddToCartButton';
+import ProductRating from '@/components/product/components/ProductRating';
+
+const ProductDetailsPage = () => {
+  return <div>ProductDetailsPage</div>;
+};
+
+export default ProductDetailsPage;
