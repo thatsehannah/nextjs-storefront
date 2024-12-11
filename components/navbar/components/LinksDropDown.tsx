01f5f83a7ba54926ac908b5468b8603ca85ba534
E. Hannah <echannah631@gmail.com>
2024-12-10 22:34:56 -0500

    creating user icon component for if user is signed in or not, displaying specific links in links dropdown based on if user is signed in our not (pretty cool)

--- components/navbar/components/LinksDropDown.tsx
+++ components/navbar/components/LinksDropDown.tsx
@@ -11,6 +11,8 @@ import { links } from '@/utils/links';
 import { Button } from '@/components/ui/button';
 import { AlignLeft } from 'lucide-react';
 import SignOutLink from './SignOutLink';
+import UserIcon from './UserIcon';
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
 
 function LinksDropdown() {
   return (
@@ -21,6 +23,7 @@ function LinksDropdown() {
           className='flex gap-4 max-w-[100px]'
         >
           <AlignLeft />
+          <UserIcon />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent
@@ -28,23 +31,38 @@ function LinksDropdown() {
         align='start'
         sideOffset={10}
       >
-        {links.map((link) => {
-          return (
-            <DropdownMenuItem key={link.href}>
-              <Link
-                href={link.href}
-                className='capitalize w-full'
-              >
-                {link.label}
-              </Link>
-            </DropdownMenuItem>
-          );
-        })}
-        <DropdownMenuSeparator />
-        {/* Temporary placeholder for logout button 👇🏾 will implement logout functionality later */}
-        <DropdownMenuItem>
-          <SignOutLink />
-        </DropdownMenuItem>
+        <SignedOut>
+          <DropdownMenuItem>
+            <SignInButton mode='modal'>
+              <button className='w-full text-left'>Log In</button>
+            </SignInButton>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <SignUpButton mode='modal'>
+              <button className='w-full text-left'>Register</button>
+            </SignUpButton>
+          </DropdownMenuItem>
+        </SignedOut>
+        <SignedIn>
+          {links.map((link) => {
+            return (
+              <DropdownMenuItem key={link.href}>
+                <Link
+                  href={link.href}
+                  className='capitalize w-full'
+                >
+                  {link.label}
+                </Link>
+              </DropdownMenuItem>
+            );
+          })}
+          <DropdownMenuSeparator />
+          {/* Temporary placeholder for logout button 👇🏾 will implement logout functionality later */}
+          <DropdownMenuItem>
+            <SignOutLink />
+          </DropdownMenuItem>
+        </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- components/navbar/components/UserIcon.tsx
+++ components/navbar/components/UserIcon.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { User2 } from 'lucide-react';
+import { currentUser, auth } from '@clerk/nextjs/server';
 
-const UserIcon = () => {
-  return <div>UserIcon</div>;
+const UserIcon = async () => {
+  const user = await currentUser();
+
+  const profileImage = user?.imageUrl;
+
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        className='w-6 h-6 rounded-full object-cover'
+      />
+    );
+  }
+
+  return <User2 className='w-8 h-8 bg-primary rounded-full text-white' />;
 };
 
 export default UserIcon;
