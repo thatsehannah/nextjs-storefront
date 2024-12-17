4e2145827ed05301a390c2bc28ceea30dacb8473
E. Hannah <echannah631@gmail.com>
2024-12-17 11:40:12 -0500

    installing and implementing faker library to speed up action submission testing

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { faker } from '@faker-js/faker';
 import { createProductAction } from '@/utils/actions';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
 const AdminCreateProductPage = () => {
+  const name = faker.commerce.productName();
+  const company = faker.company.name();
+  const description = faker.lorem.paragraph({ min: 10, max: 12 });
+
   return (
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>create product</h1>
@@ -16,6 +21,7 @@ const AdminCreateProductPage = () => {
               id='name'
               name='name'
               type='text'
+              defaultValue={name}
             />
           </div>
           <Button
