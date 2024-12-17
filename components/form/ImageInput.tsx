d6cf5ed390c01bd9e8872476be4d413f8a63f33d
E. Hannah <echannah631@gmail.com>
2024-12-17 12:07:27 -0500

    creating a image input component for loading images in create product form

---
+++ components/form/ImageInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+
+const name = 'image';
+
+const ImageInput = () => {
+  return (
+    <div className='mb-2'>
+      <Label
+        htmlFor={name}
+        className='capitalize'
+      >
+        {name}
+      </Label>
+      <Input
+        id={name}
+        name={name}
+        type='file'
+        required
+        accept='image/*'
+      />
+    </div>
+  );
+};
+
+export default ImageInput;
