54bdd2b6912cf7d40248ee181a490ca6f3758dfa
E. Hannah <echannah631@gmail.com>
2024-12-18 09:14:52 -0500

    adding comments about bug found with params object

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ProductsContainer from '@/components/products/ProductsContainer';
 
+//I introduced a bug here originally by labeling the params object as "searchParams"
+//According to Nextjs docs, it should named params, nothing else. I fixed this across
+//the application as well
+//https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#convention
 type SearchParams = {
   params: Promise<{
     layout?: 'grid' | 'list';
